c5b9643cffee0f9afb6fb32c08a9568b9f06d9e7
ProBeta1 <humblebeta@gmail.com>
2020-11-08 02:06:50 +0530

    dis calc

--- src/screens/HelpButton/HelpButton.js
+++ src/screens/HelpButton/HelpButton.js
@@ -5,6 +5,7 @@ import styles from './styles';
 import { firebase } from '../../firebase/config'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import alert from "../../pics/alert.gif";
+import * as Location from 'expo-location';
 
 const HelpButton = () => {
 
@@ -12,8 +13,64 @@ const HelpButton = () => {
   const [password, setPassword] = useState();
 
  // send the notifs to nearby with live updates
- 
+
     const  id = firebase.auth().currentUser.uid;
+
+    const rad = (x) => {
+      return x*Math.PI / 180;
+    }
+
+    useEffect(() => {
+
+  
+        (async () => {
+          let { status } = await Location.requestPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+
+          // firebase.firestore().collection("location").doc(id).set(location)
+          
+          //find nearby people
+
+          let x0 = location.coords.latitude;
+          let y0 = location.coords.longitude;
+
+          firebase.firestore().collection("location").get().then(
+            (querySnap) => {
+              querySnap.forEach(
+                (doc) => {
+                  let x = doc.coords.latitude;
+                  let y = doc.coords.longitude;
+
+                  let R = 6378137; 
+                  let dLat = rad(x - x0);
+                  let dLong = rad(y - y0);
+                  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+                    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+                  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                  let d = R * c;
+
+                   if(d<5000){
+                     // notify this one
+                     console.log("haha");
+                   } 
+
+                }
+              )
+            }
+          )
+
+
+
+        })();
+
+  
+      
+    }, []);
 
     const handleManaged = () => {
       //update everyone
